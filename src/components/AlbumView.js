69a916f5f04ef2f3b233cdd352ce7f96b1f24c4f
CRgarcia15 <cg58708@gmial.com>
2022-08-07 14:05:30 -0400

    updated and working

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
-function AlbumView() {
+function AlbumView(props) {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
     const navigate = useNavigate()
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -8,7 +8,7 @@ function ArtistView() {
     const navigate = useNavigate()
 
     useEffect(() => {
-        const API_URL = 'http://localhost:4000/album/${id}'
+        const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
             const response = await fetch(API_URL)
             const resData = await response.json()
